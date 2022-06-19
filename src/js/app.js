f9ac0e71605b771533441f99f190a219dd1429cf
Andrew Chernigovsky <chernigovsky108@gmail.com>
2022-06-19 22:12:39 +0300

    1

--- src/js/app.js
+++ src/js/app.js
@@ -80,3 +80,70 @@ import tabs from './components/tabs';
       });
   });
 })(jQuery);
+
+let urlCurrentAdress = window.location.href;
+
+let urlCurrentAdressTab1 = 'http://localhost:8080/index.html#tab1';
+let urlCurrentAdressTab2 = 'http://localhost:8080/index.html#tab2';
+let urlCurrentAdressTab3 = 'http://localhost:8080/index.html#tab3';
+let urlCurrentAdressTab4 = 'http://localhost:8080/index.html#tab4';
+
+let tab1 = document.getElementById('tab1');
+let tab2 = document.getElementById('tab2');
+let tab3 = document.getElementById('tab3');
+let tab4 = document.getElementById('tab4');
+
+let tab11 = document.getElementById('tab11');
+let tab22 = document.getElementById('tab22');
+let tab33 = document.getElementById('tab33');
+let tab44 = document.getElementById('tab44');
+
+let tab1Array = document.querySelectorAll('tab1');
+let tab1ItemsArray = document.querySelectorAll('tab1_item');
+
+if (urlCurrentAdress === urlCurrentAdressTab2) {
+  tab1.classList.add('tabActive');
+  tab11.classList.remove('active');
+  tab11.style.display = 'none';
+
+  tab2.classList.add('active');
+  tab22.classList.add('active');
+  tab22.style.display = 'block';
+}
+
+if (urlCurrentAdress === urlCurrentAdressTab3) {
+  if(tab1.classList.contains('tabActive')) {
+    tab1.classList.remove('tabActive');
+  } else {
+    tab1.classList.add('tabActive');
+  }
+
+  tab11.classList.remove('active');
+  tab11.style.display = 'none';
+
+  tab2.classList.add('tabActive');
+  tab22.classList.remove('active');
+  tab22.style.display = 'none';
+
+  tab3.classList.add('active');
+  tab33.classList.add('active');
+  tab33.style.display = 'block';
+}
+
+if (urlCurrentAdress === urlCurrentAdressTab4) {
+  tab1.classList.add('tabActive');
+  tab11.classList.remove('active');
+  tab11.style.display = 'none';
+
+  tab2.classList.add('tabActive');
+  tab22.classList.remove('active');
+  tab22.style.display = 'none';
+
+  tab3.classList.add('tabActive');
+  tab33.classList.remove('active');
+  tab33.style.display = 'none';
+
+  tab4.classList.add('active');
+  tab44.classList.add('active');
+  tab44.style.display = 'block';
+}
